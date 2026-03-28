88aa04fbb4f02f08858041feef7a5fc00474338d
Andrew Mollohan <atmollohan@gmail.com>
2026-03-27 23:11:18 -0400

    Add command tests

--- test/commands/flip.test.js
+++ test/commands/flip.test.js
@@ -1,4 +1,40 @@
+const { describe, expect, test } = require('@jest/globals');
 const flip = require('../../commands/flip');
+const unlock = require('../../commands/Functions/Achievement_Functions');
 const { assertBasicCommandModule } = require('../helpers/assertBasicCommandModule');
+const { createMessage } = require('../helpers/createMessage');
+
+jest.mock('../../commands/Functions/Achievement_Functions', () => ({
+  tracker1: jest.fn(),
+}));
 
 assertBasicCommandModule(flip, 'flip');
+
+describe('flip command', () => {
+  test('sends heads when the coin flip resolves to 0 and tracks the attempt', () => {
+    const message = createMessage();
+    message.author = { id: 'user1' };
+    const randomSpy = jest.spyOn(Math, 'random').mockReturnValue(0.1);
+
+    flip.execute(message, {}, {});
+
+    expect(message.channel.send.calls).toHaveLength(1);
+    expect(message.channel.send.calls[0]).toEqual(['Heads']);
+    expect(unlock.tracker1).toHaveBeenCalledWith('user1', 42, 1, message, {}, {});
+
+    randomSpy.mockRestore();
+  });
+
+  test('sends tails when the coin flip resolves to 1', () => {
+    const message = createMessage();
+    message.author = { id: 'user2' };
+    const randomSpy = jest.spyOn(Math, 'random').mockReturnValue(0.9);
+
+    flip.execute(message, {}, {});
+
+    expect(message.channel.send.calls).toHaveLength(1);
+    expect(message.channel.send.calls[0]).toEqual(['Tails']);
+
+    randomSpy.mockRestore();
+  });
+});
--- test/commands/names.test.js
+++ test/commands/names.test.js
@@ -1,4 +1,48 @@
+const { describe, expect, test } = require('@jest/globals');
 const names = require('../../commands/names');
+const embed = require('../../commands/Functions/embed_functions');
 const { assertBasicCommandModule } = require('../helpers/assertBasicCommandModule');
+const { createMessage } = require('../helpers/createMessage');
+
+jest.mock('../../commands/Functions/embed_functions', () => ({
+  emptyValue: 'emptyValue',
+  EmbedCreator: jest.fn((message, title, description, fields) => ({
+    title,
+    description,
+    fields,
+  })),
+}));
 
 assertBasicCommandModule(names, 'names');
+
+describe('names command', () => {
+  test('sends an embed containing the server name list', () => {
+    const message = createMessage();
+    const master = {
+      user1: { name: 'Andrew' },
+      user2: { name: 'Ben' },
+      user3: { name: 'Jack' },
+    };
+
+    names.execute(message, master);
+
+    expect(embed.EmbedCreator).toHaveBeenCalledWith(
+      message,
+      'List of all names on the Server',
+      ['Andrew', 'Ben', 'Jack'],
+      embed.emptyValue,
+    );
+    expect(message.channel.send.calls).toHaveLength(1);
+    expect(message.channel.send.calls[0]).toEqual([
+      {
+        embeds: [
+          {
+            title: 'List of all names on the Server',
+            description: ['Andrew', 'Ben', 'Jack'],
+            fields: embed.emptyValue,
+          },
+        ],
+      },
+    ]);
+  });
+});
