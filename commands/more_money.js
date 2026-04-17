b70e9a062ff720b8f0f2559726632ac5e6ae6c8d
ColinKrohto <7willythekid@gmail.com>
2026-04-17 18:54:45 -0400

    fixed bug where sending bot dms would cause an error

--- commands/more_money.js
+++ commands/more_money.js
@@ -6,7 +6,7 @@ module.exports = {
         const stats = require('./Functions/stats_functions')
         var amount = 0
         var person = message.author.id
-        
+        var dmChannelEnum = 1
         try{
             if(!master[person]){
                 //addPerson(message, master, tracker, stats_list)
@@ -16,62 +16,71 @@ module.exports = {
                     master[i].gbp = 0;
                 }
             }
-        
-            if(message.author.discriminator !== '9509' && message.author.discriminator !== '0250'){
-                if(message.content.startsWith("!") == false){
-                    if(message.channel.type !== 'dm'){
-                        if(message.member.roles.cache.find(r => r.name === "Junior Representative Assistant") || message.member.roles.cache.find(r => r.name === "Senior Representative Assistant") || message.member.roles.cache.find(r => r.name === "The People's Representative") ||message.member.roles.cache.find(r => r.name === "Dogcatcher") || message.member.roles.cache.find(r => r.name === "Soupmaker") || message.member.roles.cache.find(r => r.name === "Viceroy!")){
-                            //Large and In Charge Achievement
-                            unlock.unlock(message.author.id, 24, message, master)
-                        }
-                    }
-                    if(['590585423202484227','712755269863473252', '672846614410428416'].includes(message.channel.id) == true || message.channel.type === 'dm'){
-                        stats.tracker(person, 9, 1, stats_list)
-                    }else{
-                        stats.tracker(person, 10, 1, stats_list)
-                    }
-                   
-                    var scaling_modifier = -0.1 * master[person].gbp + 175
-                    Achievement_Switch(person, message.channel.id, message, master, tracker)
-                    Random_Achievements(person, message, master)
-                    amount = 1
-                    if(master[person].gbp < -20000){
-                        amount = 200
-                    }else if(master[person].gbp < -10000){
-                        amount = 50
-                    }else if(master[person].gbp < -2500){
-                        amount = 20
-                    }else if(master[person].gbp < -1000){
-                        amount = 10
-                    }else if(master[person].gbp < 0){
-                        amount = 5
-                        //L Achievement
-                        unlock.unlock(person, 3, message, master)
-                    }else if(master[person].gbp < 250){
-                        amount = 3
-                    }else if(master[person].gbp < 500){
-                        amount = 2
-                    }else if(master[person].gbp < 750){
-                        amount = 1
-                    }else if(master[person].gbp < 1500){
-                        var chance = Math.random() * 100
-                        if(chance > scaling_modifier){
-                            amount = 0
-                        }
-                    }else{
-                        chance = Math.random() * 100
-                        if(chance > 25){
-                            amount = 0
-                        }
-                    }
-                    if(amount == 0 && ['712755269863473252', '590585423202484227', '611276436145438769'].includes(message.channel.id) == false){
-                        if(message.channel.type !== 'dm'){
-                            amount = 1
-                        }
-                    }
-                    master[person].gbp = Math.round((parseFloat(master[person].gbp) + amount) * 100)/100
+            
+            //these are bot discriminators. should make a bot check instead of hardcoding these values tho
+            if(message.author.discriminator == '9509' && message.author.discriminator == '0250'){
+                return
+            }
+
+            //commands dont get gbp
+            if(message.content.startsWith("!")){
+                return
+            }
+
+            //1 is the enum for DM
+            if(message.channel.type !== dmChannelEnum){
+                if(message.member.roles.cache.find(r => r.name === "Junior Representative Assistant") || message.member.roles.cache.find(r => r.name === "Senior Representative Assistant") || message.member.roles.cache.find(r => r.name === "The People's Representative") ||message.member.roles.cache.find(r => r.name === "Dogcatcher") || message.member.roles.cache.find(r => r.name === "Soupmaker") || message.member.roles.cache.find(r => r.name === "Viceroy!")){
+                    //Large and In Charge Achievement
+                    unlock.unlock(message.author.id, 24, message, master)
                 }
             }
+            if(['590585423202484227','712755269863473252', '672846614410428416'].includes(message.channel.id) || message.channel.type === 'dm'){
+                stats.tracker(person, 9, 1, stats_list)
+            }else{
+                stats.tracker(person, 10, 1, stats_list)
+            }
+            
+            var scaling_modifier = -0.1 * master[person].gbp + 175
+            Achievement_Switch(person, message.channel.id, message, master, tracker)
+            Random_Achievements(person, message, master)
+            amount = 1
+            if(master[person].gbp < -20000){
+                amount = 200
+            }else if(master[person].gbp < -10000){
+                amount = 50
+            }else if(master[person].gbp < -2500){
+                amount = 20
+            }else if(master[person].gbp < -1000){
+                amount = 10
+            }else if(master[person].gbp < 0){
+                amount = 5
+                //L Achievement
+                unlock.unlock(person, 3, message, master)
+            }else if(master[person].gbp < 250){
+                amount = 3
+            }else if(master[person].gbp < 500){
+                amount = 2
+            }else if(master[person].gbp < 750){
+                amount = 1
+            }else if(master[person].gbp < 1500){
+                var chance = Math.random() * 100
+                if(chance > scaling_modifier){
+                    amount = 0
+                }
+            }else{
+                chance = Math.random() * 100
+                if(chance > 25){
+                    amount = 0
+                }
+                console.log(chance)
+            }
+            if(amount == 0 && ['712755269863473252', '590585423202484227', '611276436145438769'].includes(message.channel.id) == false){
+                //1 is dm enum
+                if(message.channel.type !== dmChannelEnum){
+                    amount = 1
+                }
+            }
+            master[person].gbp = Math.round((parseFloat(master[person].gbp) + amount) * 100)/100
         }catch(err){
             console.log(err)
             message.channel.send("Error occurred in More_money.js");
